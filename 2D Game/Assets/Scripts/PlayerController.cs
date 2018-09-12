ec954ed873c88bc90d6679e1b7ed9dbcda1e0a32
Spencer Olson <spencer.o174@stu.nebo.edu>
2018-09-12 09:30:20 -0600

    added jump

--- 2D Game/Assets/Scripts/PlayerController.cs
+++ 2D Game/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    //Player Movement Variables
+    public int MoveSpeed;
+    public float JumpHeight;
+
+    //player grounded variables
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+	void FixedUpdate()
+	{
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
 	// Update is called once per frame
 	void Update () {
-        
+        // This code makes the character jump
+        if(Input.GetKeyDown(KeyCode.Space) && grounded){
+            Jump();
+        }
 	}
+
+    public void Jump(){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+    }
+
+
 }
