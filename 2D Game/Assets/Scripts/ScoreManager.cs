7ac98ec3ace2415f7a0cf3e792f9f552a02346ed
Spencer Olson <spencer.o174@stu.nebo.edu>
2018-09-20 10:27:56 -0600

    added score

---
+++ 2D Game/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static int score;
+    Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+        scoreText = GetComponent<Text>();
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (score < 0)
+            score = 0;
+
+        scoreText.text = " " + score;
+		
+	}
+
+    public static void addPoints (int PointsToAdd) {
+        score += PointsToAdd;
+    }
+
+    public static void reset () {
+        score = 0;
+    }
+}
